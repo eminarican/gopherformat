d28c62ea94e60c939b9839b6059567ee66b8e0eb
Emin Arıcan <emin.arican@outlook.com>
2023-01-26 16:07:00 +0300

    fold groups & fix

--- src/main/kotlin/com/eminarican/gopherformat/FormatFoldingBuilder.kt
+++ src/main/kotlin/com/eminarican/gopherformat/FormatFoldingBuilder.kt
@@ -5,6 +5,7 @@ import com.intellij.lang.ASTNode
 import com.intellij.lang.folding.FoldingBuilder
 import com.intellij.lang.folding.FoldingDescriptor
 import com.intellij.openapi.editor.Document
+import com.intellij.openapi.editor.FoldingGroup
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.util.PsiTreeUtil
 
@@ -14,13 +15,15 @@ class FormatFoldingBuilder : FoldingBuilder {
         val descriptors: MutableList<FoldingDescriptor> = ArrayList()
 
         for (element in PsiTreeUtil.findChildrenOfType(node.psi, GoStringLiteral::class.java)) {
+            val foldGroup = FoldingGroup.newGroup("format_tags_fold")
+
             FormatHelper.iterate(element.text, element.textRange.startOffset) { rangeOut, _, key, offset ->
-                descriptors.add(FoldingDescriptor(element, TextRange(
+                descriptors.add(FoldingDescriptor(element.node, TextRange(
                     rangeOut.startOffset + offset, rangeOut.startOffset + key.length + 2 + offset
-                )))
-                descriptors.add(FoldingDescriptor(element, TextRange(
+                ), foldGroup, ""))
+                descriptors.add(FoldingDescriptor(element.node, TextRange(
                     rangeOut.endOffset - key.length - 3 + offset, rangeOut.endOffset + offset
-                )))
+                ), foldGroup, ""))
                 false
             }
         }
